02028537516519352011be5dae5a3f99282d9cf8
JaxonKJones <jaxonkadejones@gmail.com>
2023-03-18 17:23:21 -0600

    update obj_func

--- src/obj_func.py
+++ src/obj_func.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import cube_traction as ct 
-from meshing import x2mesh, x0_cube
+from meshing import x2mesh, x0_cube, x0_hyperboloid
 from subprocess import run, call 
 from util import *
 
@@ -13,20 +13,41 @@ def obj_func(x):
     """
     times = tic()
     # Create the mesh
-    x2mesh(x0, "cube", out_format="mesh")
+    x2mesh(x, "cube", out_format="mesh")
 
-    # evaluate mesh in FEA
+    # Evaluate mesh in FEA
     results = run(["sfepy-run", "cube_traction.py"], capture_output=False, text=True)
     print(results.stdout)
     
-    # Visualize the results
-    # run(["sfepy-view", "cube.vtk"])
+    # Calculate mass from x
+    rho = .0014 #g/mm3
+    #note: these are hardcoded for now but should be passed in somewhere for consistency
+    xres = 10
+    yres = 10
+    zres = 10
+    # spacing = (2, 2, 2)
+    x_lims = (-10, 10)
+    y_lims = (-10, 10)
+    z_lims = (-10, 10)
+
+    Lx = max(x_lims) - min(x_lims)
+    Ly = max(x_lims) - min(x_lims)
+    Lz = max(x_lims) - min(x_lims)
+
+    volume_voxel = (Lx*Ly*Lz)/ (xres*yres*zres)
+    num_voxels = Lx*Ly*Lz/volume_voxel
+    mass = rho*volume_voxel*num_voxels
+    # print(f"Volume of voxel: {volume_voxel}")
+    # print(f"Number of voxels: {num_voxels}")
+    mass = np.sum(mass)
+    # print(f"Mass: {mass} grams")
+
     toc(times, f"Total obj_func eval time", total=True)
 
-    # return max_stress
+    return mass
 
 
 if __name__ == "__main__":
-    # initial guess
-    x0 = x0_cube((20,20,20))
+    x0 = x0_cube((10,10,10)) # initial guess
     obj_func(x0)
+    run(["sfepy-view", "cube.vtk"]) # Visualize the results
